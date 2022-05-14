01c1be52c93fd899d522f158d7140e024909b6f8
Sotirios Mantziaris <smantziaris@gmail.com>
2022-05-14 09:48:58 +0300

    MQTT publisher

--- client/mqtt/integration_test.go
+++ client/mqtt/integration_test.go
@@ -5,6 +5,7 @@ package mqtt
 
 import (
 	"context"
+	"encoding/json"
 	"net/url"
 	"testing"
 
@@ -12,48 +13,59 @@ import (
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
 	"github.com/opentracing/opentracing-go/mocktracer"
+	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-const testTopic = "testTopic"
+const (
+	testTopic = "testTopic"
+	hiveMQURL = "tcp://localhost:1883"
+)
 
-func TestAsyncProducer_SendMessage_Close(t *testing.T) {
+func TestPublish(t *testing.T) {
 	mtr := mocktracer.New()
 	defer mtr.Reset()
 	opentracing.SetGlobalTracer(mtr)
 
-	u, err := url.Parse("tcp://broker.emqx.io:1883")
+	u, err := url.Parse(hiveMQURL)
 	require.NoError(t, err)
+
+	ctx, cnl := context.WithCancel(context.Background())
+	defer cnl()
 
 	cfg, err := DefaultConfig([]*url.URL{u}, "test-client-id")
 	require.NoError(t, err)
 
-	pub, err := New(cfg)
+	pub, err := New(ctx, cfg)
+	require.NoError(t, err)
+
+	payload, err := json.Marshal(struct{ Count uint64 }{Count: 1})
 	require.NoError(t, err)
 
 	msg := &paho.Publish{
 		QoS:     1,
 		Topic:   testTopic,
-		Payload: []byte("123"),
+		Payload: payload,
 	}
 
-	rsp, err := pub.Publish(context.Background(), msg)
+	rsp, err := pub.Publish(ctx, msg)
 	require.NoError(t, err)
 	assert.NotNil(t, rsp)
 
-	require.NoError(t, pub.Disconnect(context.Background()))
+	require.NoError(t, pub.Disconnect(ctx))
+
+	// Traces
 	assert.Len(t, mtr.FinishedSpans(), 1)
 
 	expected := map[string]interface{}{
-		"component": "kafka-async-producer",
+		"component": "mqtt-publisher",
 		"error":     false,
 		"span.kind": ext.SpanKindEnum("producer"),
 		"topic":     testTopic,
-		"type":      "async",
 		"version":   "dev",
 	}
 	assert.Equal(t, expected, mtr.FinishedSpans()[0].Tags())
 	// Metrics
-	// assert.Equal(t, 1, testutil.CollectAndCount(messageStatus, "component_kafka_producer_message_status"))
+	assert.Equal(t, 1, testutil.CollectAndCount(publishDurationMetrics, "client_mqtt_publish_duration_seconds"))
 }
--- client/mqtt/publisher.go
+++ client/mqtt/publisher.go
@@ -5,11 +5,35 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"strconv"
 	"time"
 
+	"github.com/beatlabs/patron/correlation"
+	"github.com/beatlabs/patron/log"
+	"github.com/beatlabs/patron/trace"
 	"github.com/eclipse/paho.golang/autopaho"
 	"github.com/eclipse/paho.golang/paho"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+const componentType = "mqtt-publisher"
+
+var publishDurationMetrics *prometheus.HistogramVec
+
+func init() {
+	publishDurationMetrics = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "client",
+			Subsystem: "mqtt",
+			Name:      "publish_duration_seconds",
+			Help:      "MQTT publish completed by the client.",
+		},
+		[]string{"topic", "success"},
+	)
+	prometheus.MustRegister(publishDurationMetrics)
+}
 
 func DefaultConfig(brokerURLs []*url.URL, clientID string) (autopaho.ClientConfig, error) {
 	if len(brokerURLs) == 0 {
@@ -19,33 +43,28 @@ func DefaultConfig(brokerURLs []*url.URL, clientID string) (autopaho.ClientConfi
 		return autopaho.ClientConfig{}, errors.New("no client id provided")
 	}
 
-	// TODO: setup callback and log info
-
 	return autopaho.ClientConfig{
 		BrokerUrls:        brokerURLs,
-		TlsCfg:            nil,
 		KeepAlive:         30,
 		ConnectRetryDelay: 5 * time.Second,
 		ConnectTimeout:    1 * time.Second,
-		WebSocketCfg:      nil,
-		OnConnectionUp:    nil,
-		OnConnectError:    nil,
-		Debug:             nil,
-		PahoDebug:         nil,
+		OnConnectionUp: func(_ *autopaho.ConnectionManager, conAck *paho.Connack) {
+			log.Infof("connection is up with reason code: %d\n", conAck.ReasonCode)
+		},
+		OnConnectError: func(err error) {
+			log.Errorf("failed to connect: %v\n", err)
+		},
 		ClientConfig: paho.ClientConfig{
-			ClientID:                   clientID,
-			Conn:                       nil,
-			MIDs:                       nil,
-			AuthHandler:                nil,
-			PingHandler:                nil,
-			Router:                     nil,
-			Persistence:                nil,
-			PacketTimeout:              0,
-			OnServerDisconnect:         nil,
-			OnClientError:              nil,
-			PublishHook:                nil,
-			EnableManualAcknowledgment: false,
-			SendAcksInterval:           0,
+			ClientID: clientID,
+			OnServerDisconnect: func(disconnect *paho.Disconnect) {
+				log.Warnf("server disconnect received with reason code: %d\n", disconnect.ReasonCode)
+			},
+			OnClientError: func(err error) {
+				log.Errorf("client error occurred: %v\n", err)
+			},
+			PublishHook: func(publish *paho.Publish) {
+				log.Debugf("message published to topic: %s\n", publish.Topic)
+			},
 		},
 	}, nil
 }
@@ -54,38 +73,80 @@ type Publisher struct {
 	cm *autopaho.ConnectionManager
 }
 
-func New(cfg autopaho.ClientConfig) (*Publisher, error) {
-	ctx, cnl := context.WithCancel(context.Background())
-	defer cnl()
-
+func New(ctx context.Context, cfg autopaho.ClientConfig) (*Publisher, error) {
 	cm, err := autopaho.NewConnection(ctx, cfg)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create connection manager: %w", err)
-	}
-
-	err = cm.AwaitConnection(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("connection is not up: %w", err)
 	}
 
 	return &Publisher{cm: cm}, nil
 }
 
 func (p *Publisher) Publish(ctx context.Context, pub *paho.Publish) (*paho.PublishResponse, error) {
+	sp, _ := trace.ChildSpan(ctx, trace.ComponentOpName(componentType, pub.Topic), componentType,
+		ext.SpanKindProducer, opentracing.Tag{Key: "topic", Value: pub.Topic})
+
+	start := time.Now()
+
 	err := p.cm.AwaitConnection(ctx)
 	if err != nil {
+		observePublish(ctx, sp, start, pub.Topic, err)
 		return nil, fmt.Errorf("connection is not up: %w", err)
 	}
 
-	// TODO: tracing, metrics
+	if err = injectObservabilityHeaders(ctx, pub, sp); err != nil {
+		observePublish(ctx, sp, start, pub.Topic, err)
+		return nil, fmt.Errorf("failed to inject tracing headers: %w", err)
+	}
+
 	rsp, err := p.cm.Publish(ctx, pub)
 	if err != nil {
+		observePublish(ctx, sp, start, pub.Topic, err)
 		return nil, fmt.Errorf("failed to publish message: %w", err)
 	}
 
+	observePublish(ctx, sp, start, pub.Topic, err)
 	return rsp, nil
 }
 
 func (p *Publisher) Disconnect(ctx context.Context) error {
 	return p.cm.Disconnect(ctx)
+}
+
+type mqttHeadersCarrier paho.UserProperties
+
+// Set implements Set() of opentracing.TextMapWriter.
+func (m *mqttHeadersCarrier) Set(key, val string) {
+	hdr := paho.UserProperties(*m)
+	hdr.Add(key, val)
+	*m = mqttHeadersCarrier(hdr)
+}
+
+func injectObservabilityHeaders(ctx context.Context, pub *paho.Publish, sp opentracing.Span) error {
+	ensurePublishingProperties(pub)
+	pub.Properties.User.Add(correlation.HeaderID, correlation.IDFromContext(ctx))
+
+	c := mqttHeadersCarrier(pub.Properties.User)
+	err := sp.Tracer().Inject(sp.Context(), opentracing.TextMap, &c)
+	return err
+}
+
+func ensurePublishingProperties(pub *paho.Publish) {
+	if pub.Properties == nil {
+		pub.Properties = &paho.PublishProperties{
+			User: paho.UserProperties{},
+		}
+	}
+	if pub.Properties.User == nil {
+		pub.Properties.User = paho.UserProperties{}
+	}
+}
+
+func observePublish(ctx context.Context, span opentracing.Span, start time.Time, topic string, err error) {
+	trace.SpanComplete(span, err)
+
+	durationHistogram := trace.Histogram{
+		Observer: publishDurationMetrics.WithLabelValues(topic, strconv.FormatBool(err == nil)),
+	}
+	durationHistogram.Observe(ctx, time.Since(start).Seconds())
 }

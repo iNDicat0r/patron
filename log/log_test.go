8fd66555d005edac7e9bcddee2c69b90e5ed3fc6
Mobin Hosseini <m.hosseini@thebeat.co>
2022-06-17 11:17:23 +0300

    Add metrics test for fmtLogger

--- log/log_test.go
+++ log/log_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,6 +31,42 @@ func TestLevelOrder(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 			assert.Equal(t, tt.want, LevelOrder(tt.args.lvl))
+		})
+	}
+}
+
+func TestLogMetrics(t *testing.T) {
+	t.Parallel()
+	LogCounter.Reset()
+	l = &fmtLogger{}
+	tests := map[string]struct {
+		lvl      Level
+		logfunc  func(args ...interface{})
+		logfuncf func(msg string, args ...interface{})
+	}{
+		"debug": {lvl: DebugLevel, logfunc: l.Debug, logfuncf: l.Debugf},
+		"info":  {lvl: InfoLevel, logfunc: l.Info, logfuncf: l.Infof},
+		"warn":  {lvl: WarnLevel, logfunc: l.Warn, logfuncf: l.Warnf},
+		"error": {lvl: ErrorLevel, logfunc: l.Error, logfuncf: l.Errorf},
+		"panic": {lvl: PanicLevel, logfunc: l.Panic, logfuncf: l.Panicf},
+	}
+	for name, tt := range tests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, 0.0, testutil.ToFloat64(LogCounter.WithLabelValues(string(tt.lvl))))
+			if tt.lvl == PanicLevel {
+				assert.Panics(t, func() { tt.logfunc(name) })
+			} else {
+				tt.logfunc(name)
+			}
+			assert.Equal(t, 1.0, testutil.ToFloat64(LogCounter.WithLabelValues(string(tt.lvl))))
+			if tt.lvl == PanicLevel {
+				assert.Panics(t, func() { tt.logfuncf(name) })
+			} else {
+				tt.logfuncf(name)
+			}
+			assert.Equal(t, 2.0, testutil.ToFloat64(LogCounter.WithLabelValues(string(tt.lvl))))
 		})
 	}
 }
